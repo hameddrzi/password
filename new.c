5600e390ea886417acea2cc2c91cbbe32758c581
Hamed <hamed.2012.darzi@gmail.com>
2024-08-21 22:31:28 +0200

    baba eyvolla

---
+++ new.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#define MAX_C 20
+
+
+int main() {
+
+    char password[MAX_C+ 1];
+    scanf("%20s", password);
+
+
+    bool esiste_cifre = false ; //P1
+
+    for (size_t i = 0; password[i] && !esiste_cifre ; ++i) {
+        if(isdigit(password[i]))
+            esiste_cifre = true;
+    }
+
+    bool lettere = false;  //P2
+    for (size_t i = 0; password[i] && !lettere ; ++i) {
+        if(!(isalpha(password[i])))
+            esiste_cifre = true;
+    }
+
+    bool  due_maiuscole = false ;  //P3
+    for (size_t i = 0; password[i] && !due_maiuscole ; ++i) {
+        if(2 > (isupper(password[i])))
+            due_maiuscole = true;
+    }
+
+    bool cifre_consentive = false;
+    if(password[0] != '\0'){
+        for (size_t i = 0; password[i] && !cifre_consentive ; ++i) {
+            if(isdigit(password[i]) && isdigit(password[i -1]))
+                cifre_consentive = true;
+        }
+    }
+    size_t len = 0;
+    while (password[len]){
+        len++;
+    }
+    bool ultimi_cifre = (len >= 2) &&
+                        ispunct(password[len-1]) &&
+                        ispunct(password[len -2]);
+   printf("hasan dardashti");
+}
